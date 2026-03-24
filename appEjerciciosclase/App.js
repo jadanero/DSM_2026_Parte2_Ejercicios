8c1c6af8bb20e6addf61fa572dd229b2b5136147
jadanero <jadaneroc@gmail.com>
2026-03-24 18:06:14 +0100

    Ej1

---
+++ appEjerciciosclase/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Text onPress={() => alert("Hola Mundo")}>
+        Click me!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
